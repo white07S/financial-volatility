527fabbdf7a969382ceaea1514d04aad5f174c8f
white07S <sharmapreetam.uk@outlook.com>
2022-09-16 11:57:32 +0200

    docker tested

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import File
+from fastapi import FastAPI
+from fastapi import UploadFile
+import numpy as np
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome from the API"}
