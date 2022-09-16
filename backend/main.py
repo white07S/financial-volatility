954fffcf221f7d093709bcf7dfb207d081580a1d
white07S <sharmapreetam.uk@outlook.com>
2022-09-16 12:33:32 +0200

    crud and models added

--- backend/main.py
+++ backend/main.py
@@ -11,3 +11,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Welcome from the API"}
+
+
+
+@app.post("/volatilityest/{symbol}/{est}")
+def volest():
+    return {"volest":"findall"}
+
+
